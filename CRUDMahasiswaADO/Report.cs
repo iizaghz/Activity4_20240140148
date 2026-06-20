c418b52312aa3462dc6e8bc2b70e427ed26e0029
iizaghz <sukmahawaiza@gmail.com>
2026-06-20 11:35:42 +0700

    feat: implement student data reporting feature with filtering and Crystal Reports integration

--- CRUDMahasiswaADO/Report.cs
+++ CRUDMahasiswaADO/Report.cs
@@ -8,53 +8,32 @@ namespace CRUDMahasiswaADO
 {
     public partial class Report : Form
     {
-        static string connectionString =
-            "Data Source=IZAYAAA\\IZA;Initial Catalog=DBAkademikADO;Integrated Security=True";
-
-        SqlConnection conn = new SqlConnection(connectionString);
-        SqlDataAdapter da;
-        DataTable dtMahasiswa;
+        DAL dbLogic = new DAL();
 
         CrystalReport1 listMahasiswa = new CrystalReport1();
 
         string prodi { get; set; }
-        DateTime tglMasuk { get; set; }
+        DateTime tglmasuk { get; set; }
 
         public Report(string Prodi, DateTime TglMasuk)
         {
             InitializeComponent();
 
             prodi = Prodi;
-            tglMasuk = TglMasuk;
+            tglmasuk = TglMasuk;
 
             try
             {
-                if (conn.State == ConnectionState.Closed)
-                {
-                    conn.Open();
-                }
-
-                SqlCommand cmd = new SqlCommand("sp_Report", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                cmd.Parameters.AddWithValue("@inProdi", prodi);
-                cmd.Parameters.AddWithValue("@inTglMsuk", tglMasuk.Year);
-
-                da = new SqlDataAdapter(cmd);
-
-                dtMahasiswa = new DataTable();
-                da.Fill(dtMahasiswa);
-
-                conn.Close();
+                DataTable dtMahasiswa = dbLogic.getDataRekap(prodi, tglmasuk);
 
                 listMahasiswa.SetDataSource(dtMahasiswa);
-
                 crystalReportViewer1.ReportSource = listMahasiswa;
                 crystalReportViewer1.Refresh();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Gagal load data : " + ex.Message);
+                //simpanLog(ex.Message);
+                MessageBox.Show("Gagal load data: " + ex.Message);
             }
         }
     }
